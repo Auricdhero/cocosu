34e5d8cbaacc1dfc9706d2ee205885cbf2c92d1a
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2024-11-01 06:29:03 +0000

    created media controls

--- pages/sermons/[slug].vue
+++ pages/sermons/[slug].vue
@@ -1,30 +1,29 @@
 <template>
     <div>
-        <v-parallax :src="config.public.strapi.url + sermon.data.attributes.Picture.data.attributes.url"
-            height="290">
+        <v-parallax :src="config.public.strapi.url + sermon.data.attributes.Picture.data.attributes.url" height="290">
             <div style="background-color: rgba(52, 67, 238, 0.555); height: 100%;">
                 <v-container>
-                    <h1 class="d-flex justify-center mt-16 text-white">{{ sermon.data.attributes.Title }}</h1>
+                    <h1 class="d-flex justify-center mt-10 text-white">{{ sermon.data.attributes.Title }}</h1>
                     <p class="text-muted text-center text-white">{{ sermon.data.attributes.Sermonist }}</p>
                 </v-container>
                 <audio ref="audio">
                     <source :src="config.public.strapi.url + sermon.data.attributes.Audio.data.attributes.url">
                 </audio>
-                <v-row>
-                    <v-col>
-                        <div v-if="play==true">
-                            <v-btn icon="mdi-pause" variant="text" @click="pause" color="white"></v-btn>
-                        </div>
-                        <div v-else>
-                            <v-btn icon="mdi-play" variant="text" @click="play" color="white"></v-btn>
-                        </div>
-                    </v-col>
-                    <v-col>
-                        <v-btn class="d-flex justify-right" icon="mdi-download" @click="openFile" variant="text" color="white"></v-btn>
-                    </v-col>
-                    <br>
-                    
-                </v-row>
+                <v-container>
+                    <!-- <span>{{ currentTime }}/{{ duration }}</span> -->
+                    <v-slider :min="currentTime" :max="duration" thumb-label color="white"></v-slider>
+                    <v-row>
+                        <v-col>
+                            <v-btn icon="mdi-play-circle-outline" variant="text" @click="playing =! playing" color="white"></v-btn>
+                        </v-col>
+                        <v-col>
+                            <v-btn class="d-flex justify-right" icon="mdi-download" @click="openFile" variant="text"
+                                color="white"></v-btn>
+                        </v-col>
+                        <br>
+                    </v-row>
+                </v-container>
+
             </div>
         </v-parallax>
         <v-container>
@@ -33,13 +32,16 @@
     </div>
 </template>
 <script setup>
+import { useMediaControls } from '@vueuse/core';
 const route = useRoute();
 const config = useRuntimeConfig();
 import Markdown from 'vue3-markdown-it';
-const audio = ref(null)
+const audio = ref()
 
 
 const { data: sermon } = await useFetch(config.public.strapi.url + '/api/sermons/' + route.params.slug + '?populate=*');
+
+const {playing, duration, currentTime, buffered} = useMediaControls(audio,{src: config.public.strapi.url + sermon.value.data.attributes.Audio.data.attributes.url})
 // console.log(sermon)
 
 const play = function () {
