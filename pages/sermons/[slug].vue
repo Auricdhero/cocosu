376fb30df6862a13f90471e0830879fafe4b2046
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2024-11-02 12:55:21 +0000

    debuging slider

--- pages/sermons/[slug].vue
+++ pages/sermons/[slug].vue
@@ -7,21 +7,30 @@
                     <p class="text-muted text-center text-white">{{ sermon.data.attributes.Sermonist }}</p>
                 </v-container>
                 <audio ref="audio">
-                    <source :src="config.public.strapi.url + sermon.data.attributes.Audio.data.attributes.url">
+                    <!-- <source :src="config.public.strapi.url + sermon.data.attributes.Audio.data.attributes.url"> -->
                 </audio>
                 <v-container>
                     <!-- <span>{{ currentTime }}/{{ duration }}</span> -->
-                    <v-slider :min="currentTime" :max="duration" thumb-label color="white"></v-slider>
-                    <v-row>
-                        <v-col>
-                            <v-btn icon="mdi-play-circle-outline" variant="text" @click="playing =! playing" color="white"></v-btn>
-                        </v-col>
-                        <v-col>
-                            <v-btn class="d-flex justify-right" icon="mdi-download" @click="openFile" variant="text"
-                                color="white"></v-btn>
-                        </v-col>
-                        <br>
-                    </v-row>
+                    <v-slider v-model="currentTime" :value="useDateFormat(duration, timeFormat)"
+                        :min="useDateFormat(currentTime, timeFormat)" :max="useDateFormat(duration, timeFormat)"
+                        :step="stepper" thumb-label color="white"></v-slider>
+                    <div class="d-flex justify-center mx-auto">
+                        <v-row>
+                            <v-col cols="5"></v-col>
+                            <v-col cols="1">
+                                <v-btn v-if="!playing" icon="mdi-play-circle-outline" variant="text"
+                                    @click="playing = !playing" color="white"></v-btn>
+                                <v-btn @click="playing = !playing" variant="text" color="white"
+                                    icon="mdi-pause-circle-outline" v-else></v-btn>
+                            </v-col>
+                            <v-col cols="1">
+                                <v-btn class="d-flex justify-right" icon="mdi-download" @click="openFile" variant="text"
+                                    color="white"></v-btn>
+                            </v-col>
+                            <v-col cols="3"></v-col>
+                            <br>
+                        </v-row>
+                    </div>
                 </v-container>
 
             </div>
@@ -32,24 +41,24 @@
     </div>
 </template>
 <script setup>
-import { useMediaControls } from '@vueuse/core';
+import { useMediaControls, useDateFormat, useNow } from '@vueuse/core';
+import Markdown from 'vue3-markdown-it';
 const route = useRoute();
 const config = useRuntimeConfig();
-import Markdown from 'vue3-markdown-it';
 const audio = ref()
 
 
 const { data: sermon } = await useFetch(config.public.strapi.url + '/api/sermons/' + route.params.slug + '?populate=*');
 
-const {playing, duration, currentTime, buffered} = useMediaControls(audio,{src: config.public.strapi.url + sermon.value.data.attributes.Audio.data.attributes.url})
+const { playing, duration, currentTime, buffered} = useMediaControls(audio, { src: config.public.strapi.url + sermon.value.data.attributes.Audio.data.attributes.url })
 // console.log(sermon)
+const timeFormat = ref('mm:ss');
 
-const play = function () {
-    audio.value.play();
-};
-const pause = function () {
-    audio.value.pause();
-};
+// const stepper = useDateFormat(duration, timeFormat)
+
+function formatDuration(seconds) {
+    return new Date(1000 * seconds).toISOString().slice(14, 19)
+}
 function openFile() {
     open(config.public.strapi.url + sermon.attributes.Slides.data.attributes.url)
 }
